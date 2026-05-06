ce6984fa8789b5c6cf53a9c3455b590b40a1d044
Tosha <toshanidutta@gmail.com>
2026-05-06 15:39:50 +0530

    added UC7

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,11 +1,41 @@
+import java.util.Random;
 
 public class tictactoe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
-        placeMove(0, 0, 'X');
-        System.out.println("Value at board[0][0]: " + board[0][0]);
+        computerMove();
+    }
+
+    static void computerMove() {
+        Random random = new Random();
+        int slot, row, col;
+        boolean moved = false;
+
+        System.out.println("Computer is making a move...");
+
+        do {
+            slot = random.nextInt(9) + 1;
+            row = (slot - 1) / 3;
+            col = (slot - 1) % 3;
+
+            if (isValidMove(row, col)) {
+                placeMove(row, col, computerSymbol);
+                System.out.println("Computer placed '" + computerSymbol + "' in slot " + slot);
+                moved = true;
+            }
+            
+        } while (!moved);
+    }
+
+    static boolean isValidMove(int row, int col) {
+        return board[row][col] == '-';
     }
 
     static void placeMove(int row, int col, char symbol) {
