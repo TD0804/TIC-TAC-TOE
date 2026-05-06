c9d23009cbdad508ea6423d9cebe6639349d45f5
Tosha <toshanidutta@gmail.com>
2026-05-06 15:16:12 +0530

    added UC4

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,19 +1,18 @@
-import java.util.Scanner;
-
 
 public class tictactoe {
 
     public static void main(String[] args) {
-        int slot = getUsersSlot();
-        System.out.println("Slot entered: " + slot);
+        int slot = 7;
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
     }
 
-    static int getUsersSlot() {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter a slot number (1-9): ");
-        
-        int slot = sc.nextInt();
-        return slot;
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
     }
 }
