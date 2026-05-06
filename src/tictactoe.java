52235b9a91b2da7319fea34db117ab46851ccdbe
Tosha <toshanidutta@gmail.com>
2026-05-06 16:32:02 +0530

    added UC9

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,25 +1,29 @@
 
 public class tictactoe {
 
-    static boolean isHumanTurn = true;
-    static boolean gameOver = false;
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        
-        while (!gameOver) {
-            
-            if (isHumanTurn) {
-                System.out.println("--- Human Player's Turn ---");
-                isHumanTurn = false; 
-            } else {
-                System.out.println("--- Computer's Turn ---");
-                // computerMove();
-                
-                isHumanTurn = true;
+        System.out.println("Has 'X' won? " + hasWon('X'));
+    }
+
+    static boolean hasWon(char symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
+                return true;
             }
-            gameOver = true; 
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) {
+                return true;
+            }
         }
-        
-        System.out.println("Game Over!");
+
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+            return true;
+        }
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+            return true;
+        }
+
+        return false;
     }
 }
