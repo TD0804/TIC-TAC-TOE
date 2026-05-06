e18bd16efe00daa063791fa7f87d4bbf6dace08c
Tosha <toshanidutta@gmail.com>
2026-05-06 16:02:35 +0530

    added UC8

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,44 +1,25 @@
-import java.util.Random;
 
 public class tictactoe {
 
-    static char[][] board = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
-    };
-    static char computerSymbol = 'O';
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
 
     public static void main(String[] args) {
-        computerMove();
-    }
-
-    static void computerMove() {
-        Random random = new Random();
-        int slot, row, col;
-        boolean moved = false;
-
-        System.out.println("Computer is making a move...");
-
-        do {
-            slot = random.nextInt(9) + 1;
-            row = (slot - 1) / 3;
-            col = (slot - 1) % 3;
-
-            if (isValidMove(row, col)) {
-                placeMove(row, col, computerSymbol);
-                System.out.println("Computer placed '" + computerSymbol + "' in slot " + slot);
-                moved = true;
-            }
+        
+        while (!gameOver) {
             
-        } while (!moved);
-    }
-
-    static boolean isValidMove(int row, int col) {
-        return board[row][col] == '-';
-    }
-
-    static void placeMove(int row, int col, char symbol) {
-        board[row][col] = symbol;
+            if (isHumanTurn) {
+                System.out.println("--- Human Player's Turn ---");
+                isHumanTurn = false; 
+            } else {
+                System.out.println("--- Computer's Turn ---");
+                // computerMove();
+                
+                isHumanTurn = true;
+            }
+            gameOver = true; 
+        }
+        
+        System.out.println("Game Over!");
     }
 }
